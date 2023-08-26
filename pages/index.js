bc4eeef2307f41436766c73697bd2cc1928b2d3c
nampd <nampd@hpt.vn>
2023-08-26 17:18:38 +0700

    Final Pet project Next.js

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 import Alert from '../components/alert';
 import Link from 'next/link';
+import Date from '../components/date';
 
 import { getSortedPostsData } from '../lib/posts';
 
@@ -38,11 +39,13 @@ export default function Home({ allPostsData }) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
-              <br />
-              {id}
-              <br />
-              {date}
+              <li className={utilStyles.listItem} key={id}>
+                <Link href={`/posts/${id}`}>{title}</Link>
+                <br />
+                <small className={utilStyles.lightText}>
+                  <Date dateString={date} />
+                </small>
+              </li>
             </li>
           ))}
         </ul>
